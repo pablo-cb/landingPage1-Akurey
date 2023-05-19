4112e6dee9c98683ea8197bc959c7bec6b28a249
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-05-19 17:06:10 -0600

    I am separating the reusable sections into separate css files, also, changing nomenclature and global styles

---
+++ script.js
@@ -0,0 +1,141 @@
+var button1 = document.getElementById("button1");
+if (button1) {
+  button1.addEventListener("click", function () {
+    window.open("https://akurey.com/");
+  });
+}
+
+var button2 = document.getElementById("button2");
+if (button2) {
+  button2.addEventListener("click", function () {
+    window.open("https://calendly.com/rquesada-ak/30min?month=2023-04");
+  });
+}
+
+var properWorkloadManagement = document.getElementById(
+  "properWorkloadManagement"
+);
+if (properWorkloadManagement) {
+  properWorkloadManagement.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='properWorkloadManagement']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var defineRolesAnd = document.getElementById("defineRolesAnd");
+if (defineRolesAnd) {
+  defineRolesAnd.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='defineRolesAnd']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var adherenceToIndustry = document.getElementById("adherenceToIndustry");
+if (adherenceToIndustry) {
+  adherenceToIndustry.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='adherenceToIndustry']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var securityConsiderations = document.getElementById("securityConsiderations");
+if (securityConsiderations) {
+  securityConsiderations.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='securityConsiderations']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var availabilityOfExperienced = document.getElementById(
+  "availabilityOfExperienced"
+);
+if (availabilityOfExperienced) {
+  availabilityOfExperienced.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='availabilityOfExperienced']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var considerCulturalFit = document.getElementById("considerCulturalFit");
+if (considerCulturalFit) {
+  considerCulturalFit.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='considerCulturalFit']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var distanceBetweenTeams = document.getElementById("distanceBetweenTeams");
+if (distanceBetweenTeams) {
+  distanceBetweenTeams.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='distanceBetweenTeams']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start" });
+    }
+  });
+}
+
+var button11 = document.getElementById("button11");
+if (button11) {
+  button11.addEventListener("click", function () {
+    window.open("https://calendly.com/rquesada-ak/30min?month=2023-04");
+  });
+}
+
+var button21 = document.getElementById("button21");
+if (button21) {
+  button21.addEventListener("click", function () {
+    window.open("https://akurey.com/");
+  });
+}
+
+var button = document.getElementById("button");
+if (button) {
+  button.addEventListener("click", function () {
+    window.open("https://calendly.com/rquesada-ak/30min?month=2023-04");
+  });
+}
+
+var facebookIcon = document.getElementById("facebookIcon");
+if (facebookIcon) {
+  facebookIcon.addEventListener("click", function () {
+    window.open("https://www.facebook.com/akureyinc/");
+  });
+}
+
+var linkedInIcon = document.getElementById("linkedInIcon");
+if (linkedInIcon) {
+  linkedInIcon.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/company/akureyinc/mycompany/");
+  });
+}
+
+var instagramIcon = document.getElementById("instagramIcon");
+if (instagramIcon) {
+  instagramIcon.addEventListener("click", function () {
+    window.open("https://www.instagram.com/akureyinc/");
+  });
+}
